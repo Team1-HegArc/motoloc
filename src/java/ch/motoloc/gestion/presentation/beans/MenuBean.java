a5396cd10f1b22ef597b6b3e6c5c7a9de2b8d4cb
freyer <bibien.limido@157.26.105.240>
2015-02-25 15:09:36 +0100

    MenuNav Final

--- src/java/ch/motoloc/gestion/presentation/beans/MenuBean.java
+++ src/java/ch/motoloc/gestion/presentation/beans/MenuBean.java
@@ -3,44 +3,65 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package ch.motoloc.gestion.presentation.beans;
 
-import java.beans.*;
-import java.io.Serializable;
+import java.io.IOException;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
 
 /**
  *
  * @author bibien.limido
  */
-public class MenuBean implements Serializable {
-    
-    public static final String PROP_SAMPLE_PROPERTY = "sampleProperty";
-    
-    private String sampleProperty;
-    
-    private PropertyChangeSupport propertySupport;
-    
+@ManagedBean(name = "menuBean")
+@RequestScoped
+public class MenuBean {
+
+    @ManagedProperty("#{param.pageId}")
+    String pageId;
+
+    public String getPageId() {
+        return pageId;
+    }
+
+    public void setPageId(String pageId) {
+        this.pageId = pageId;
+    }
+
+    /**
+     * Creates a new instance of MenuBean
+     */
     public MenuBean() {
-        propertySupport = new PropertyChangeSupport(this);
     }
-    
-    public String getSampleProperty() {
-        return sampleProperty;
+
+    public void affichePage() throws IOException {
+
+        switch (pageId) {
+
+            case "accueil":
+
+                FacesContext.getCurrentInstance().getExternalContext().redirect("accueil.xhtml");
+
+                break;
+            case "ajoutClient":
+                FacesContext.getCurrentInstance().getExternalContext().redirect("ajoutClient.xhtml");
+                break;
+            case "ajoutMoto":
+                FacesContext.getCurrentInstance().getExternalContext().redirect("ajoutMoto.xhtml");
+                break;
+            case "listeClients":
+                FacesContext.getCurrentInstance().getExternalContext().redirect("listeClients.xhtml");
+                break;
+            case "listeMotos":
+                FacesContext.getCurrentInstance().getExternalContext().redirect("listeMotos.xhtml");
+                break;
+
+            default:
+                FacesContext.getCurrentInstance().getExternalContext().redirect("accueil.xhtml");
+
+        }
     }
-    
-    public void setSampleProperty(String value) {
-        String oldValue = sampleProperty;
-        sampleProperty = value;
-        propertySupport.firePropertyChange(PROP_SAMPLE_PROPERTY, oldValue, sampleProperty);
-    }
-    
-    public void addPropertyChangeListener(PropertyChangeListener listener) {
-        propertySupport.addPropertyChangeListener(listener);
-    }
-    
-    public void removePropertyChangeListener(PropertyChangeListener listener) {
-        propertySupport.removePropertyChangeListener(listener);
-    }
-    
+
 }
