dc5c05db54e73256ac536efebee7df1469475e2a
Administrateur <Administrateur@PdevGr1>
2015-02-25 14:52:43 +0100

    JPA / Irina

--- src/java/ch/motoloc/gestion/business/FactureForfait.java
+++ src/java/ch/motoloc/gestion/business/FactureForfait.java
@@ -1,8 +1,6 @@
 package ch.motoloc.gestion.business;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 import javax.persistence.PrimaryKeyJoinColumn;
--- src/java/ch/motoloc/gestion/business/FactureReservation.java
+++ src/java/ch/motoloc/gestion/business/FactureReservation.java
@@ -1,9 +1,7 @@
 package ch.motoloc.gestion.business;
 
 import java.util.List;
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
