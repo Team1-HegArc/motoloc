f65ef0e11e854b2c8223a507c10d9ecb8922c379
vincent robatel <vincentrobatel@157.26.111.229>
2015-02-25 15:02:17 +0100

    Ajout jpa forfait

--- src/java/ch/motoloc/gestion/business/Pack.java
+++ src/java/ch/motoloc/gestion/business/Pack.java
@@ -5,6 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 
@@ -17,24 +18,8 @@ import javax.persistence.Table;
 @Table(name="RENTAL_PACK")
 public class Pack extends Forfait {
     
-    @Id
-    @Column(name="PK_RENTAL_PACK")
-    private Long id;
-    
-    @ManyToOne
-    @JoinColumn(name="FK_MOTORBIKE_MODEL")
-    private MotoModele motoModele;
-    
-    // TABLE PACK_TYPE
-    @Column(name = "NB_DAYS")
-    private int nbJours;
-    @Column(name="NB_WEEKENDS")
-    private int nbWeekends;
-    
-    // TABLE PACK
-    @Column(name="BASIC_PRICE")
-    private Double prix_base;
-    @Column(name="KM_PRICE")
-    private Double prix_km;
+    @OneToOne
+    @JoinColumn(name="FK_PACK", unique=true)
+    private TarificationPack tarificationPack; 
 
 }
