0cfcb9508a680b399c2670a5a2d64862e41527fa
lucas.hornisbe <lucas.hornisbe@EGE-HORNISBERGL.intra.eiaj.ch>
2015-02-25 14:16:34 +0100

    Serice Client

---
+++ src/java/ch/motoloc/gestion/services/ClientService.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.motoloc.gestion.services;
+
+import ch.motoloc.gestion.business.Client;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+
+public class ClientService {
+
+    private static List<Client> listClients = new ArrayList();
+
+    public static Client ajouterClient(String nom, String prenom, String rue, String npa,
+            String ville, Date dateDeNaissance, String email, String telephone, String remarque, String numeroPermis) {
+
+        Client client = new Client(nom, prenom, rue, npa, ville, dateDeNaissance, email, telephone, remarque, numeroPermis);
+        listClients.add(client);
+        return client;
+    }  
+           
+               
+
+    public static void sauverClient(Long id, String nom, String prenom, String rue, String npa, 
+            String ville, Date dateDeNaissance, String email, String telephone, String remarque, String numeroPermis){
+        for (Client c : listClients) {
+            if (c.getId().equals(id)) {
+                c.setPrenom(prenom);
+                c.setNom(nom);
+                c.setRue(rue);
+                c.setNpa(npa);
+                c.setVille(ville);
+                c.setDateDeNaissance(dateDeNaissance);
+                c.setEmail(email);
+                c.setTelephone(telephone);
+                c.setRemarque(remarque);
+                c.setNumeroPermis(numeroPermis);
+            }
+        }
+    }
+
+
+
+}
