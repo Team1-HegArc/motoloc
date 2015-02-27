f221f956e3647248058f151639e9b5bfb9853f44
vincent robatel <vincentrobatel@157.26.105.204>
2015-02-27 16:48:16 +0100

    ADD dao CLientv2

--- src/java/ch/motoloc/gestion/services/ClientService.java
+++ src/java/ch/motoloc/gestion/services/ClientService.java
@@ -1,94 +1,55 @@
 package ch.motoloc.gestion.services;
 
 import ch.motoloc.gestion.business.Client;
-import ch.motoloc.gestion.persistence.ClientDAO;
 import ch.motoloc.gestion.persistence.JpaConnection;
+import ch.motoloc.gestion.persistence.dao.ClientDAO;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import javax.persistence.EntityManager;
 
 public class ClientService {
 
-    private static List<Client> listClients = new ArrayList();
-
-    public static boolean ajouterClient(Client client) {
-        try {
-
-            listClients.add(client);
-//                JpaConnection.getEntityManager().getTransaction().begin();
-//                ClientDAO.create(client);
-//                JpaConnection.getEntityManager().getTransaction().commit();
-            return true;
-
-        } catch (Exception e) {
-            return false;
-        }
-
-    }
-
     public static boolean sauverClient(Client client) {
         boolean success = false;
+        EntityManager em = JpaConnection.getEntityManager();
         try {
-
+            em.getTransaction().begin();
             if (client.getId() != null) {
-
-                for (Client c : listClients) {
-                    if (c.getId().equals(client.getId())) {
-                        c = client;
-                JpaConnection.getEntityManager().getTransaction().begin();
-                JpaConnection.getEntityManager().getTransaction().commit();
-                        success = true;
-
-                    }
-                }
+                Client clientToUpdate = ClientDAO.getInstance().findById(client.getId());
+                clientToUpdate.setNom(client.getNom());
             } else {
-                 JpaConnection.getEntityManager().getTransaction().begin();
-                 ClientDAO.create(client);
-                 JpaConnection.getEntityManager().getTransaction().commit();
-                listClients.add(client);
-                success = true;
+                em.persist(client);
             }
-
+            em.getTransaction().commit();
+            success = true;
         } catch (Exception e) {
+            em.getTransaction().rollback();
         }
         return success;
     }
 
-    public static List<Client> getAllClients() {
-        /*
-         ajoute en dur pour test
-         */
-
-        listClients.add(new Client("bibien", "bibien", "bibien", "bibien", "bibien", null, "dsa@sd.com", "0798976767", null, null));
-        listClients.add(new Client("bibien", "bibien", "bibien", "bibien", "bibien", null, "dsa@sd.com", "0798976767", null, null));
-
-//return ClientDAO.findAll();     
-        return listClients;
+    public static List<Client> getAllClients() {     
+        return ClientDAO.getInstance().findAll();
     }
 
     public static boolean supprimerClient(Client client) {
         boolean success = false;
-
+        EntityManager em = JpaConnection.getEntityManager();
         try {
-            for (int i = 0; i < listClients.size(); i++) {
-                if (listClients.get(i).getId().equals(client.getId())) {
-                    JpaConnection.getEntityManager().getTransaction().begin();
-                    JpaConnection.getEntityManager().remove(client);
-                    JpaConnection.getEntityManager().getTransaction().commit();
-                    listClients.remove(i);
-                    success = true;
-                }
-            }
-
-            return success;
+            em.getTransaction().begin();
+            em.remove(client);
+            em.getTransaction().commit();
+            success = true;
         } catch (Exception e) {
-            return success;
+            em.getTransaction().rollback();
         }
+        return success;
 
     }
 
-    public static Client getClientId() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public static Client getClientId(Long id) {
+        return ClientDAO.getInstance().findById(id);
     }
 
     public static Client rechercherClient(String prenom, String nom, String email) {
