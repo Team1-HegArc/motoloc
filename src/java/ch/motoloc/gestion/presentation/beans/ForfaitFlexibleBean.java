2e2ae23ac587040ec4a812e45371c6a328637f07
vincent robatel <vincentrobatel@157.26.105.229>
2015-03-03 09:10:09 +0100

    ajout flexible v2

---
+++ src/java/ch/motoloc/gestion/presentation/beans/ForfaitFlexibleBean.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.motoloc.gestion.presentation.beans;
+
+import ch.motoloc.gestion.business.ForfaitFlexible;
+
+/**
+ *
+ * @author vincentrobatel
+ */
+public class ForfaitFlexibleBean {
+    
+    private ForfaitFlexible forfaitFlexible;
+
+    public ForfaitFlexibleBean() {
+    }
+
+    public ForfaitFlexible getForfaitFlexible() {
+        return forfaitFlexible;
+    }
+
+    public void setForfaitFlexible(ForfaitFlexible forfaitFlexible) {
+        this.forfaitFlexible = forfaitFlexible;
+    }
+    
+   
+}
