f392e33a2a0f381d037430ccf26bda27cbe66402
Perfidious <popo.tom@gmail.com>
2015-03-04 20:02:04 +0100

    ReservationDAO

--- src/java/ch/motoloc/gestion/persistence/dao/ReservationDAO.java
+++ src/java/ch/motoloc/gestion/persistence/dao/ReservationDAO.java
@@ -33,12 +33,12 @@ public class ReservationDAO extends AbstractDAO<Reservation>{
     public List<Reservation> findAllMotoAvailable(Reservation reservation, Moto moto ){
         StringBuilder sb = new StringBuilder();
         
-                  sb.append("SELECT res FROM Reservation res WHERE (?3) = res.moto.id ")
-           .append("AND (?1) = res.dateDebut ")
-           .append("AND (?2) = res.dateFin ")
-           .append("AND (?1) BETWEEN res.dateDebut AND res.dateFin ")
-           .append("AND (?2) BETWEEN res.dateDebut AND res.dateFin ")
-           .append("AND (?1) >= CURRENT_DATE").toString();
+                  sb.append("SELECT res FROM Reservation res WHERE ?3 = res.moto.id ")
+           .append("AND ?1 = res.dateDebut ")
+           .append("AND ?2 = res.dateFin ")
+           .append("AND ?1 BETWEEN res.dateDebut AND res.dateFin ")
+           .append("AND ?2 BETWEEN res.dateDebut AND res.dateFin ");
+           //.append("AND (?1) >= CURRENT_DATE");
         
          EntityManager em = JpaConnection.getEntityManager();
          
