f7920a58053979f4251cbac29a9cace4ac74e2d3
Perfidious <popo.tom@gmail.com>
2015-03-04 14:04:10 +0100

    Reservation v1

--- src/java/ch/motoloc/gestion/presentation/beans/ReservationBean.java
+++ src/java/ch/motoloc/gestion/presentation/beans/ReservationBean.java
@@ -1,42 +1,48 @@
 package ch.motoloc.gestion.presentation.beans;
 
 import ch.motoloc.gestion.business.Forfait;
+import ch.motoloc.gestion.business.Moto;
 import ch.motoloc.gestion.business.Reservation;
 import ch.motoloc.gestion.services.ConvertisseurDate;
 import ch.motoloc.gestion.services.ReservationService;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.model.ListDataModel;
 
 /**
  * Bean ReservationBean
+ *
  * @author irina.fessemaz
  */
 @ManagedBean(name = "reservationBean")
 @SessionScoped
 public class ReservationBean {
-    
+
     private Reservation reservation;
-    
+    private ListDataModel<Moto> motos;
     private String dateDebut;
     private String dateFin;
-    
 
     public ReservationBean() {
     }
-    
+
     public String ajouterReservation(Forfait forfait) {
         this.reservation = new Reservation();
         forfait.addReservation(reservation);
         return "choixDate";
     }
-    
+
     public String rechercherDispoMoto(Reservation reservation) {
         this.reservation.setDateDebut(ConvertisseurDate.formatterDate(this.dateDebut));
         this.reservation.setDateFin(ConvertisseurDate.formatterDate(this.dateFin));
+
+        motos = new ListDataModel();
+        motos.setWrappedData(ReservationService.rechercherDispoMoto(reservation));
         
+
         return "rechercherDispoMoto";
     }
-    
+
     public String sauverReservation() {
         ReservationService.sauverReservation(reservation);
         return "succes";
@@ -65,7 +71,5 @@ public class ReservationBean {
     public void setDateFin(String dateFin) {
         this.dateFin = dateFin;
     }
-    
-    
-    
+
 }
