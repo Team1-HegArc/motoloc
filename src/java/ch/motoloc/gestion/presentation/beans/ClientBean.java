b3d34b43a19b17003188a9ee1802e39f7bcdb801
Administrateur <Administrateur@PdevGr1>
2015-03-04 04:47:39 +0100

    / Irina

--- src/java/ch/motoloc/gestion/presentation/beans/ClientBean.java
+++ src/java/ch/motoloc/gestion/presentation/beans/ClientBean.java
@@ -6,81 +6,78 @@ import java.io.Serializable;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
-
 /**
  *
  * @author cyril.briguet
  */
 @ManagedBean(name = "clientBean")
 @SessionScoped
-public class ClientBean implements Serializable{
+public class ClientBean implements Serializable {
 
     private Client client;
     private boolean erreur = false;
 
-    
     public ClientBean() {
     }
-    
-    public String ajouterClient (){
+
+    public String ajouterClient() {
         this.client = new Client();
         return "editClient";
     }
-    
 
-    public String editerClient (Client client){
+    public String editerClient(Client client) {
         this.client = client;
         return "editClient";
     }
-    
 
-    public String sauverClient(){
+    public String sauverClient() {
         boolean checkAjout = ClientService.sauverClient(client);
-        if(!checkAjout) {
+        if (!checkAjout) {
             erreur = true;
-            return "erreur"; 
+            return "erreur";
         } else {
             erreur = false;
             return "succes";
         }
     }
 
-    public String supprimerClient (Client client){
+    public String supprimerClient(Client client) {
         boolean checkSuppression = ClientService.supprimerClient(client);
-        if(!checkSuppression) {
+        if (!checkSuppression) {
             erreur = true;
-            return "erreur"; 
+            return "erreur";
         } else {
             erreur = false;
             return "succes";
         }
     }
-    
-    public String detailClient (Client client){
-        this.client=client;
+
+    public String detailClient(Client client) {
+        this.client = client;
         return "detailClient";
     }
-    
-        public String detailForfaitPack(Client client){
-        this.client=client;
+
+    public String detailForfaitPack(Client client) {
+        this.client = client;
         return "detailForfaitPack";
     }
     
+    public String detailForfaitFlexible(Client client) {
+        this.client = client;
+        return "detailForfaitFlexible";
+    }
 
     public Client getClient() {
         return client;
     }
 
- 
     public void setClient(Client client) {
         this.client = client;
     }
-    
 
     public boolean isErreur() {
         return erreur;
     }
-
 
     public void setErreur(boolean erreur) {
         this.erreur = erreur;
