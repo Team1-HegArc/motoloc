127633d7d5e1e4653f452eb95d1e38f7ddb2588d
Administrateur <Administrateur@PdevGr1>
2015-03-04 05:28:15 +0100

    / Irina

--- src/java/ch/motoloc/gestion/presentation/beans/ReservationBean.java
+++ src/java/ch/motoloc/gestion/presentation/beans/ReservationBean.java
@@ -2,6 +2,7 @@ package ch.motoloc.gestion.presentation.beans;
 
 import ch.motoloc.gestion.business.Forfait;
 import ch.motoloc.gestion.business.Reservation;
+import ch.motoloc.gestion.services.ConvertisseurDate;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
@@ -15,13 +16,27 @@ public class ReservationBean {
     
     private Reservation reservation;
     
+    private String dateDebut;
+    private String dateFin;
+    
 
     public ReservationBean() {
     }
     
     public String ajouterReservation(Forfait forfait) {
         this.reservation = new Reservation();
-        forfait.addReservation(reservation);
+        forfait.getReservations().add(reservation);
+        return "succes";
+    }
+    
+    public String rechercherDispoMoto(Forfait forfait) {
+        this.reservation.setDateDebut(ConvertisseurDate.formatterDate(this.dateDebut));
+        this.reservation.setDateFin(ConvertisseurDate.formatterDate(this.dateFin));
+        return "rechercherDispoMoto";
+    }
+    
+    public String sauverReservation() {
+        //
         return "succes";
     }
 
@@ -32,5 +47,23 @@ public class ReservationBean {
     public void setReservation(Reservation reservation) {
         this.reservation = reservation;
     }
+
+    public String getDateDebut() {
+        return dateDebut;
+    }
+
+    public void setDateDebut(String dateDebut) {
+        this.dateDebut = dateDebut;
+    }
+
+    public String getDateFin() {
+        return dateFin;
+    }
+
+    public void setDateFin(String dateFin) {
+        this.dateFin = dateFin;
+    }
+    
+    
     
 }
